b679d9ca493e286686ca63deb7544b4d1a5cf678
Pierre Bossis <pbossis@hotmail.com>
2024-01-13 12:24:02 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GenshinIonic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
