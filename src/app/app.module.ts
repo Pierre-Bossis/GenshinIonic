a8bb8ed6340f631437f32c6de825858f096a060c
Pierre Bossis <pbossis@hotmail.com>
2024-01-20 15:48:26 +0100

    version stable + firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -9,10 +9,16 @@ import { AppRoutingModule } from './app-routing.module';
 import {  HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { TokenInterceptorService } from './_services/interceptors/token-interceptor.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,IonicStorageModule.forRoot(),HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,IonicStorageModule.forRoot(),HttpClientModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi:true}
